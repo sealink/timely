f17cd309268648ee4a7cd170fb7a447c95f8bc76
Michael Noack <michael@noack.com.au>
2012-10-09 12:58:20 +1030

    Add date range validity module

--- lib/timely/rails.rb
+++ lib/timely/rails.rb
@@ -2,3 +2,4 @@ require 'timely/rails/extensions'
 ActiveRecord::Base.extend Timely::Extensions
 require 'timely/rails/season'
 require 'timely/rails/date_group'
+require 'timely/rails/date_range_validity_module'
---
+++ lib/timely/rails/date_range_validity_module.rb
@@ -0,0 +1,27 @@
+module Timely
+  module DateRangeValidityModule
+    def self.included(base)
+      base.class_eval do
+        validates :from, :to, :presence => true
+      end
+    end
+
+    def validity_range
+      (from .. to)
+    end
+
+    def correctness_of_date_range
+      if (from.present? && to.present?)
+        errors.add(:base, "Invalid Date Range. From date should be less than or equal to To date") if from > to
+      end
+    end
+
+    def validity_range_to_s
+      "#{from.to_s(:short)} ~ #{to.to_s(:short)}"
+    end
+    
+    def valid_on?(date)
+      validity_range.include?(date)
+    end
+  end
+end
