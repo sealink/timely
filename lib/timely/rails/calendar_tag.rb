7b7d0bd4ef461cbcc5e807cc2dee299b9c0b6043
john.mortlock <john.mortlock@gmail.com>
2022-04-22 15:35:29 +0930

    Handle to_s deprecation in Rails7

--- lib/timely/rails/calendar_tag.rb
+++ lib/timely/rails/calendar_tag.rb
@@ -11,7 +11,9 @@ module Timely
       def calendar_tag(name, value = Timely.current_date, *args)
         options = args.extract_options!
 
-        value = value.to_s(:calendar) if value.respond_to?(:day)
+        if value.respond_to?(:day)
+          value = value.respond_to?(:to_fs) ? value.to_fs(:calendar) : value.to_s(:calendar)
+        end
 
         name = name.to_s if name.is_a?(Symbol)
 
