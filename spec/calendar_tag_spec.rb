dcb7ae2a36a74dc607d3ec1d11fad404a152b830
john.mortlock <john.mortlock@gmail.com>
2022-04-23 08:58:16 +0930

    Fix issue with rspec 5.1 not matching the response, despite no diff

--- spec/calendar_tag_spec.rb
+++ spec/calendar_tag_spec.rb
@@ -17,13 +17,13 @@ describe Timely::ActionViewHelpers do
   it 'should generate calendar tags' do
     expect(string).to receive(:html_safe)
     expect(subject).to receive(:tag).with(:input,
-                                          id: 'test',
+                                          hash_including(id: 'test',
                                           class: 'datepicker',
                                           size: 10,
                                           maxlength: 10,
                                           name: 'test',
                                           type: 'text',
-                                          value: '2000-12-25').and_return(string)
+                                          value: '2000-12-25')).and_return(string)
     subject.calendar_tag :test
   end
 end
